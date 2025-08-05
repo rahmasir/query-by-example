9c8f60e7049c8f276d49c04f779488b39ea1420d
EnAnsari <rahmat.ansari.dev@gmail.com>
2025-08-05 13:15:37 +0330

    update: some changes happended

--- src/main/java/org/rahmasir/querybyexample/employee/EmployeeRepository.java
+++ src/main/java/org/rahmasir/querybyexample/employee/EmployeeRepository.java
@@ -2,9 +2,11 @@ package org.rahmasir.querybyexample.employee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long>, QueryByExampleExecutor<Employee> {
 
     List<Employee> findAllByFirstName(String firstName);
